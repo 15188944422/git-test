9305872aa4d1322a4dadfa91d3726ee67e77cb32
Song <2268992162@qq.com>
2022-09-22 14:05:02 +0800

    master--->test02

--- git-demo01/src/main/java/com/git/song/GitTest.java
+++ git-demo01/src/main/java/com/git/song/GitTest.java
@@ -3,5 +3,7 @@ package com.git.song;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("master--->test01");
+        System.out.println("master--->test02");
+
     }
 }
